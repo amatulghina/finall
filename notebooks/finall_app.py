5a330299121f3936e6393a12d725ef7b0ac0080d
Aisyah Amatul Ghina <amatulghina@gmail.com>
2024-09-23 21:47:56 +0200

    Begin finall_app.py

---
+++ notebooks/finall_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import matplotlib
+
+
+st.write("""
+# Welcome to FinAll Project!
+
+Financial Education for All
+""")
+
