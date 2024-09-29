0ba1bc9f167c9a70e67741c4ee6c38f7b1376c19
Aisyah Amatul Ghina <amatulghina@gmail.com>
2024-09-29 22:07:10 +0200

    Updated

--- finall_app/finall_app.py
+++ finall_app/finall_app.py
@@ -24,9 +24,10 @@ client = gspread.authorize(scoped_credentials)
 # Open the Google Sheet by name
 sheet = client.open("finall_project").worksheet("visitor")
 
+im = Image.open("../img/finall-favicon-color.png")
 st.set_page_config(
     page_title="FinAll",
-    page_icon="https://github.com/amatulghina/finall/blob/master/img/finall-favicon-color.png"
+    page_icon=im
 )
 
 st.write("""
