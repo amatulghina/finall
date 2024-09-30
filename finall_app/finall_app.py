b4c1e2b451b1f2d8072ad0398156750089aa8e1b
Aisyah Amatul Ghina <amatulghina@gmail.com>
2024-10-01 00:39:49 +0200

    Updated

--- finall_app/finall_app.py
+++ finall_app/finall_app.py
@@ -29,8 +29,8 @@ sheet2 = client.open("finall_project").worksheet("stock_index")
 home = st.Page("finall_app.py", title="Home")
 company_analysis = st.Page("Company_Analysis.py", title="Company Analysis")
 
-pg = st.navigation([home, company_analysis])
-st.set_page_config(page_title="FinAll")
+#pg = st.navigation([home, company_analysis])
+#st.set_page_config(page_title="FinAll")
 #pg.run()
 
 
