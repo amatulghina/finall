9abd8b3cfdd8a2180936c80b655822ffaf112fb1
Aisyah Amatul Ghina <amatulghina@gmail.com>
2024-10-01 00:48:57 +0200

    Updated

--- finall_app/finall_app.py
+++ finall_app/finall_app.py
@@ -51,9 +51,9 @@ Please fill the form below:
 
 with st.form("User Profile"):
     st.header("User Profile Information")
-    country = st.selectbox("Country (Required)", ["France", "Germany", "Spain"], index=None, placeholder="Select your country")
+    country = st.selectbox("Country (Required)", ["France", "Germany", "Spain"], index=None, placeholder="Select your country",)
     name = st.text_input("Name")
-    age = st.number_input("Age (Required)", min=15, max=100)
+    age = st.number_input("Age (Required)", min_value=15, max_value=100)
     gender = st.selectbox("Gender (Required)", ["Male", "Female", "Other"], index=None)
     
     # Risk Profiling Section
