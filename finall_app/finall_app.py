b2045cc244e8ad034a10e4646b0dab3e58d1e5e2
Aisyah Amatul Ghina <amatulghina@gmail.com>
2024-10-01 21:59:59 +0200

    Updated

--- finall_app/finall_app.py
+++ finall_app/finall_app.py
@@ -33,7 +33,9 @@ sheet2 = client.open("finall_project").worksheet("stock_index")
 st.set_page_config(page_title="FinAll")
 #pg.run()
 
-st.logo("https://github.com/amatulghina/finall/blob/master/img/finall-high-resolution-logo-transparent.png")
+st.write("""
+# FinAll Project
+""")
 
 
 st.write("""
