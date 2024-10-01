87fe72cb288d8d9e74be6b370878ee593660057b
Aisyah Amatul Ghina <amatulghina@gmail.com>
2024-10-01 21:40:11 +0200

    Updated

--- finall_app/finall_app.py
+++ finall_app/finall_app.py
@@ -41,10 +41,13 @@ Financial Education for All
 
 
 st.write("""
-Introduction content (to be developed)
+As we progress through various stages of life, our needs and goals naturally evolve. It’s crucial to have extra resources available to cushion the impact of economic downturns or unexpected financial shocks, such as job loss, car accidents, divorce, or the death of a spouse, to prevent these events from becoming catastrophic. Through careful financial planning, we can navigate difficult times and thrive during prosperous periods. Preparing in advance, such as by setting up an emergency fund or building a savings and investment portfolio, helps safeguard both ourselves and our families from financial setbacks. \n
 
-In order to give you personalized financial advice, we would like to know you more.
-Please fill the form below:
+Despite the historical evidence that stock markets generate superior long-term returns compared to other financial instruments such as bonds and savings accounts, individual and household participation in stock markets remains low in many European countries (ie., 23%, 13%, and 13% for Germany, France and Spain, respectively). Research shows that financial literacy is one of significant factor to the reluctancy of individuals to invest in the stock market. \n
+
+This platform is built to increase financial literacy in stock market from personal finance perspective. \n
+
+In order to give you personalized financial advice, we would like to know you more. Please fill the form below:
 
 """)
 
