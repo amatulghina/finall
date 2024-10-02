f3d3cf5a24f071ed289c4939579e5b8f2670f8f1
Aisyah Amatul Ghina <amatulghina@gmail.com>
2024-10-02 07:13:49 +0200

    Updated

--- finall_app/finall_app.py
+++ finall_app/finall_app.py
@@ -337,7 +337,7 @@ Based on this rule, your investment portfolio may consists of :blue[**{100-age}%
         """)
         
         if 'button' not in st.session_state:
-        st.session_state.button = False
+            st.session_state.button = False
 
         def click_button():
             st.session_state.button = not st.session_state.button
