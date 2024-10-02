e1eea0730038837a0d86ca50c49540e47cf48e85
Aisyah Amatul Ghina <amatulghina@gmail.com>
2024-10-02 22:50:53 +0200

    Updated

--- finall_app/pages/Stock_Price_Prediction.py
+++ finall_app/pages/Stock_Price_Prediction.py
@@ -15,6 +15,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 st.set_page_config(page_title="FinAll")
 
