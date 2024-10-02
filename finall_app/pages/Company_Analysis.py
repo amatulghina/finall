7f773715a1b58a2b3214a645d66294cb1392bc8d
Aisyah Amatul Ghina <amatulghina@gmail.com>
2024-10-02 15:17:42 +0200

    Updated

--- finall_app/pages/Company_Analysis.py
+++ finall_app/pages/Company_Analysis.py
@@ -176,7 +176,7 @@ if st.button("Submit"):
     
     st.write("Technical analysis, on the other hand, involves analyzing **historical price** data and trading **volume** to predict future stock movements. It focuses on price patterns and market trends rather than the underlying value of the company. Typically used by traders for **short-term** trades but can also be applied to medium-term and long-term investments.")
     
-    st.write(f"The two graphs below show the closing price and volume of {ticker}")
+    st.write(f"The two graphs below show the **closing price** and **trading volume** of {ticker}")
     
     # Download Historical Data
     data_tickers = yf.Tickers(ticker)
