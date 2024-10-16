82e324a0b4284636e03dae50a890d2a2bf7d0158
Aisyah Amatul Ghina <amatulghina@gmail.com>
2024-10-16 14:57:01 +0200

    Updated

--- finall_app/pages/Privacy_Policy.py
+++ finall_app/pages/Privacy_Policy.py
@@ -5,22 +5,21 @@ st.set_page_config(page_title="FinAll")
 st.write("""
 # Privacy Policy
 
-At finall.streamlit.app, we respect your privacy and are committed to protecting your personal data. This Privacy Policy explains how we collect, use, and safeguard your information when you use our web app.
+At **FinAll**, we respect your privacy and are committed to protecting your personal data. This Privacy Policy explains how we collect, use, and safeguard your information when you use our web app.
 
-**1. Data We Collect**
-We collect minimal personal data to provide and improve our services:
-Personal Information: This may include your name which are collected when you voluntarily provide them.
+**1. Data We Collect** \n
+We collect minimal personal data to provide and improve our services. This may include your name which are collected when you voluntarily provide them.
 
-**2. How We Use Your Data**
+**2. How We Use Your Data** \n
 We use your data for the following purposes:
 To provide and maintain the functionality of the app.
 To respond to your inquiries or support requests.
 To analyze usage patterns and improve the app’s performance.
 
-**3. Data Sharing**
+**3. Data Sharing** \n
 We do not sell or share your personal data with third parties.
 
-**4. User Rights**
+**4. User Rights** \n
 You have the following rights under GDPR:
 Access: You can request access to the personal data we hold about you.
 Correction: You can ask us to correct any inaccurate information.
@@ -28,15 +27,15 @@ Deletion: You can request the deletion of your personal data.
 Objection/Restriction: You can object to or restrict certain data processing activities.
 To exercise your rights, please contact us at [amatulghina@gmail.com].
 
-**5. Data Security**
+**5. Data Security** \n
 We take the security of your data seriously and protect your personal information from unauthorized access or breaches.
 
-**6. Data Retention**
+**6. Data Retention** \n
 We retain your personal data only as long as necessary to fulfill the purposes outlined in this policy or to comply with legal obligations.
 
-**8. Changes to This Policy**
+**8. Changes to This Policy** \n
 We may update this Privacy Policy periodically. Any changes will be posted on this page.
 
-**9. Contact Us**
+**9. Contact Us** \n
 If you have any questions about this Privacy Policy or your personal data, please contact us at [contact email].
 """)
